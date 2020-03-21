5e91a1da407d87492c16afb6a2e99f968b7e9481
amriagus <amriagus45@gmail.com>
2020-03-21 23:26:42 +0700

    publish file 04Latihan

---
+++ 04Latihan10.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Penggunaan For</title>
+</head>
+<body>
+<?Php
+echo "Tabel Perkalian<br>";
+echo "===============<br>";
+ For ($ni = 15; $ni<100; $ni++)
+ {
+ 	$kali = 12*$ni;
+ 	Print("12 x $ni = $kali<br>\n");
+ }
+ ?>
+</body>
+</html> 
